051472d6f8297520bcc9b93b775e50b598615529
David Mccurdy <dmccurdy@ucsc.edu>
2024-05-13 14:56:45 -0700

    pushing upodates for meeting

--- testorbkit.py
+++ testorbkit.py
@@ -66,11 +66,10 @@ import orbkit as ok
 # inso = ok.insolation(kyear = (10,0), latitude = None, season = None, days = None, show_plot = 'On', output_type= 'array')
 
 
-inso = ok.insolation(kyear = [6000,1000,0], show_plot= 'On', output_type='global annual mean')
+#inso = ok.insolation(kyear = [6000,1000,0], show_plot= 'On', output_type='global annual mean')
 
 
 #gmi = ok.insolation(kyear = (6000,0), output_type='global annual mean')
 
-breakpoint()
 
 
